fa2edf58eeb21e11c7e93f583a9ce36ad6a7656d
Md. Jakaria Hosen <maxjakaria825@gmail.in>
2024-03-31 00:31:01 +0600

    Creat RecipeDetailsPage

---
+++ lib/RecipeDetailsPage.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class RecipeDetailsPage extends StatelessWidget {
+  final dynamic recipe;
+
+  RecipeDetailsPage({required this.recipe});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(recipe['strMeal']),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            recipe['strMealThumb'] != null
+                ? Image.network(
+                    recipe['strMealThumb'],
+                    loadingBuilder: (context, child, loadingProgress) {
+                      if (loadingProgress == null) return child;
+                      return CircularProgressIndicator();
+                    },
+                    errorBuilder: (context, error, stackTrace) {
+                      return Text('Failed to load image');
+                    },
+                  )
+                : SizedBox.shrink(),
+            SizedBox(height: 16.0),
+            Text(
+              recipe['strMeal'],
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              'Ingredients:',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8.0),
+            for (int i = 1; i <= 20; i++)
+              recipe['strIngredient$i'] != null
+                  ? Text(
+                      '- ${recipe['strIngredient$i']} ${recipe['strMeasure$i']}')
+                  : SizedBox.shrink(),
+            SizedBox(height: 16.0),
+            Text(
+              'Instructions:',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8.0),
+            Text(recipe['strInstructions']),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/RecipeListPage.dart
+++ lib/RecipeListPage.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:my_recipe_app/RecipeDetailsPage.dart';
 
 class RecipeListPage extends StatefulWidget {
   @override
@@ -71,64 +72,6 @@ class _RecipeListPageState extends State<RecipeListPage> {
             ],
           );
         },
-      ),
-    );
-  }
-}
-
-class RecipeDetailsPage extends StatelessWidget {
-  final dynamic recipe;
-
-  RecipeDetailsPage({required this.recipe});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(recipe['strMeal']),
-      ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            recipe['strMealThumb'] != null
-                ? Image.network(
-                    recipe['strMealThumb'],
-                    loadingBuilder: (context, child, loadingProgress) {
-                      if (loadingProgress == null) return child;
-                      return CircularProgressIndicator();
-                    },
-                    errorBuilder: (context, error, stackTrace) {
-                      return Text('Failed to load image');
-                    },
-                  )
-                : SizedBox.shrink(),
-            SizedBox(height: 16.0),
-            Text(
-              recipe['strMeal'],
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 16.0),
-            Text(
-              'Ingredients:',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 8.0),
-            for (int i = 1; i <= 20; i++)
-              recipe['strIngredient$i'] != null
-                  ? Text(
-                      '- ${recipe['strIngredient$i']} ${recipe['strMeasure$i']}')
-                  : SizedBox.shrink(),
-            SizedBox(height: 16.0),
-            Text(
-              'Instructions:',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 8.0),
-            Text(recipe['strInstructions']),
-          ],
-        ),
       ),
     );
   }
